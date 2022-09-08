1d7a0a2839a17ccd9032fc0ffc3cee0a358b9fd6
Bailey Cage <bailey.cage@gmail.com>
2022-09-08 14:16:20 -0400

    display cards for all toys

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -2,8 +2,14 @@ import React from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer({ toysAry }) {
+
+  const toyCards = toysAry.map(toy => {
+    return (
+      <ToyCard key={toy.id} toy={toy} />
+    )
+  })
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toyCards}</div>
   );
 }
 
