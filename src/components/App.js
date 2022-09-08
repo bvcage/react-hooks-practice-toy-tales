313daca44cde783fb51e418b8c17804113545a90
Bailey Cage <bailey.cage@gmail.com>
2022-09-08 14:12:21 -0400

    fetch toys from db

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
+
+  const [toysAry, setToysAry] = useState([]);
   const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/toys`)
+    .then(r => r.json())
+    .then(data => setToysAry(data))
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
@@ -18,7 +26,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toysAry={toysAry} />
     </>
   );
 }
